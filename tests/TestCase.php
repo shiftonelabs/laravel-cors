6fd09805e7cb4673016ddc491702fef77a660ccc
Patrick Carlo-Hickman <patrick.carlohickman@gmail.com>
2019-12-20 13:58:30 -0500

    Remove unneeded usage of Dotenv.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,6 @@
 
 namespace ShiftOneLabs\LaravelCors\Tests;
 
-use Exception;
-use Dotenv\Dotenv;
 use Illuminate\Config\Repository;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -21,9 +19,6 @@ class TestCase extends BaseTestCase
         $app->singleton(\Illuminate\Contracts\Http\Kernel::class, \Illuminate\Foundation\Http\Kernel::class);
         $app->singleton(\Illuminate\Contracts\Console\Kernel::class, \Illuminate\Foundation\Console\Kernel::class);
 
-        // bootstrap: load environment
-        $this->loadEnvironment();
-
         // bootstrap: register config
         $app->instance('config', $config = new Repository([]));
 
@@ -34,23 +29,5 @@ class TestCase extends BaseTestCase
         $app->boot();
 
         return $app;
-    }
-
-    /**
-     * Load the environment variables from the .env file.
-     *
-     * @return void
-     */
-    public function loadEnvironment()
-    {
-        try {
-            if (method_exists(Dotenv::class, 'create')) {
-                Dotenv::create(__DIR__.'/..')->safeLoad();
-            } else {
-                (new Dotenv(__DIR__.'/..'))->load();
-            }
-        } catch (Exception $e) {
-            //
-        }
     }
 }
